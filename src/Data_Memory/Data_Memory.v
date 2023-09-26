0380bf796d4f7c94a7cf45003915202171159545
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2023-09-26 10:12:24 +0300

    Update Data_Memory.v

--- src/Data_Memory/Data_Memory.v
+++ src/Data_Memory/Data_Memory.v
@@ -6,6 +6,11 @@ WD,
 output reg[31:0]RD,
 output [15:0]test_value);
 reg[31:0]memoryData[0:255];
+  integer i;
+initial begin  
+           for(i=0;i<256;i=i+1)  
+                memoryData[i] <= 32'd0;  
+      end  
 always @(posedge clk)
 begin
 if(WE)
