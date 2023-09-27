68dfc8297603db8b61c16526dd4875feac9c5008
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2023-09-27 09:56:39 +0300

    Update SignExtend.v

--- src/Data_path/SignExtend.v
+++ src/Data_path/SignExtend.v
@@ -2,5 +2,5 @@ module SignExtend(
 input [15:0]inp,
 output [31:0]extendOutput
     );
-    assign extendOutput={{16{extendOutput[15]}},inp};
+    assign extendOutput={{16{inp[15]}},inp};
 endmodule
