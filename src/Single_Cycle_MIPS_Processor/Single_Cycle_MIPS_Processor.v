837acb22899ae9eb9462e0b3ccb4bc20dcb5c6fa
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2023-09-27 09:57:24 +0300

    Update Single_Cycle_MIPS_Processor.v

--- src/Single_Cycle_MIPS_Processor/Single_Cycle_MIPS_Processor.v
+++ src/Single_Cycle_MIPS_Processor/Single_Cycle_MIPS_Processor.v
@@ -1,46 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2023 07:27:43 AM
+// Design Name: 
+// Module Name: Single_Cycle_MIPS_Processor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module Single_Cycle_MIPS_Processor(
 input clk,
-input areset
+input areset,
+output [15:0]test_value,
+output [31:0]pc_out,
+instr,
+output      MemtoReg,MemWrite,Branch,ALUSrc,RegDst,RegWrite,jump
+
 
     );
-    wire [31:0]RD;
     
-    wire [2:0]ALUCtrl;
-    wire [4:0]A3;
+    wire [2:0]ALUCtrl;    
     wire [31:0]WriteData;
-    wire [31:0]extendOutput;
-    wire [31:0]shiftOut;
-    wire [31:0]SrcB;
+    
     wire [31:0]ALUResult;
     wire Zero;
-    wire [15:0]test_value;
     wire [31:0]ReadData;
-    wire [31:0]Result;
-    wire [31:0]pc_out;
-    wire [31:0]PCPlus4,PCBranch,PC_in1,PC_in;
     wire PCSrc;
-    wire [25:0]shiftOut1;
-    wire MemtoReg,MemWrite,Branch,ALUSrc,RegDst,RegWrite,jump;
+     
+    wire [31:0]PCPlus4;
+     
 
-    ControlUnit CU(.Zero(Zero),.opcode(RD[31:26]),.funct(RD[5:0]),
+    ControlUnit CU(.Zero(Zero),.opcode(instr[31:26]),.funct(instr[5:0]),
        .MemtoReg1(MemtoReg),.MemWrite1(MemWrite)
        ,.Branch1(Branch),
        .ALUSrc1(ALUSrc),.RegDst1(RegDst),.PCSrc(PCSrc),
        .RegWrite1(RegWrite),.jump1(jump),.ALUCtrl(ALUCtrl));
        
        
-    Data_Path dp(.clk( clk),.reset(areset),
-    .ALUControl(ALUCtrl),
-    .MemtoReg(MemtoReg),.Branch(Branch),.ALUSrc(ALUSrc)
+    Data_Path dp(.PCPlus4(PCPlus4),.clk(clk),.reset(areset),.ALUControl(ALUCtrl),
+    .MemtoReg(MemtoReg),.ALUSrc(ALUSrc),.PCSrc(PCSrc)
     ,.RegDst(RegDst),.RegWrite(RegWrite),.jump(jump),
     .ReadData(ReadData),
-    .instr(RD[25:0]),
-    .PC_out(pc_out), .WriteData(),.ALUOut(ALUResult),
+    .instr(instr),
+    .PC_out(pc_out), .WriteData(WriteData),.ALUOut(ALUResult),
     .Zero(Zero));
-   instruction_memory imem(.A(pc_out),.RD(RD));  
+   instruction_memory imem(.A(pc_out),.RD(instr));  
    
    
-   Data_Memory datamemory(.clk(clk),.WE(MemWrite),
+   Data_Memory datamemory(.clk(clk),.reset(areset),.WE(MemWrite),
    .A(ALUResult),.WD(WriteData),.test_value(test_value),.RD(ReadData));
   
   
